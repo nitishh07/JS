0b8bd5a078c5457c201b51cd4122e1acd7a80e15
Nitish Kumar <nitishkumar@kiit.ac.in>
2025-12-23 22:50:36 +0530

    Update dates.js

--- basics/dates.js
+++ basics/dates.js
@@ -6,5 +6,26 @@ let myDate = new Date()
 // console.log(typeof myDate)
 // console.log(myDate.toLocaleString())
 
-let myCreatedDate = new Date(2026, 0 , 26)
-console.log(myCreatedDate.toDateString())
+// let myCreatedDate = new Date(2026, 0 , 26)
+
+// let myCreatedDate = new Date(2026, 0 , 23 , 5 , 3)
+
+let myCreatedDate = new Date("01-01-2026")
+console.log(myCreatedDate.toLocaleString());
+
+//ye abhi ka aa gya ms me
+let myTimeStamp = Date.now()
+console.log(myTimeStamp)
+console.log(myCreatedDate.getTime())
+
+//to convert into sec
+console.log((Math.floor(Date.now())/1000))
+
+
+let newDate = new Date()
+console.log(newDate)
+console.log(newDate.getMonth() + 1)
+
+newDate.toLocaleString('default', {
+    weekday: "long",
+});
