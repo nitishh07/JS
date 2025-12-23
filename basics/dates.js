ebb5cadb7a99b208213856a03c6e03e23e603ce7
Nitish Kumar <nitishkumar@kiit.ac.in>
2025-12-23 22:36:11 +0530

    Updated dates.js

--- basics/dates.js
+++ basics/dates.js
@@ -1,6 +1,10 @@
 //Dates
 
 let myDate = new Date()
-console.log(myDate.toString())
-console.log(myDate.toDateString())
-console.log(typeof myDate)
+// console.log(myDate.toString())
+// console.log(myDate.toDateString())
+// console.log(typeof myDate)
+// console.log(myDate.toLocaleString())
+
+let myCreatedDate = new Date(2026, 0 , 26)
+console.log(myCreatedDate.toDateString())
