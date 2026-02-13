a0ec6138782dd7b48bc354e0facdb77612f8f586
Nitish Kumar <nitishkumar@kiit.ac.in>
2026-02-13 23:35:07 +0530

    Update

---
+++ events/three.js
@@ -0,0 +1,17 @@
+const sayDate = function(str){
+    console.log(str, Date.now());
+}
+
+let intervalId = null;   // const nahi, let
+
+document.querySelector("#start").addEventListener('click', function(){
+    if(intervalId === null){
+        intervalId = setInterval(sayDate, 1000, "Pihu");
+    }
+});
+
+document.querySelector("#stop").addEventListener('click', function(){
+    clearInterval(intervalId);
+    intervalId = null;
+});
+
